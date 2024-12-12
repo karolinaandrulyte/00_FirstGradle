6cbd8e4079b1c37afcf07ddb906cd15ce549fc10
Karolina Andrulyte <karolina.andrulyte@gmail.com>
2024-12-12 15:29:15 +0200

    Changed hardcoded path to inputStream for environment compatibility, project is running on Docker

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,9 @@
 package org.example;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Scanner;
 
 import org.example.entities.DoublyLinkedList;
@@ -10,13 +12,15 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String filePath = "C:\\Users\\karandr\\IdeaProjects\\00_FirstGradle\\src\\main\\resources\\text.txt";
-
         DoublyLinkedList<String> list = new DoublyLinkedList<>();
 
         try {
-            File myObj = new File(filePath);
-            Scanner myReader = new Scanner(myObj);
+            InputStream inputStream = Main.class.getClassLoader().getResourceAsStream("text.txt");
+            if (inputStream == null) {
+                throw new FileNotFoundException("Resource 'text.txt' not found.");
+            }
+
+            Scanner myReader = new Scanner(inputStream);
             while (myReader.hasNextLine()) {
                 String data = myReader.nextLine();
                 list.insertAtEnd(data);
@@ -24,20 +28,17 @@ public class Main {
             }
             myReader.close();
 
-
         } catch (IOException e) {
             System.out.println("IOException occurred.");
             e.printStackTrace();
         }
 
         list.get(2);
-
         list.delete(1);
         list.traverseForward();
 
         System.out.println();
         System.out.println(list);
-
     }
 
 }
